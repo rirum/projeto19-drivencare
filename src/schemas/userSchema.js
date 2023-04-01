4e43c1b2c4cf3f6582a0ea33da274d83c2b93d9e
camille <cerulean.dsgn@gmail.com>
2023-04-01 12:46:42 -0300

    fix: required on schema

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -19,8 +19,8 @@ export const doctorSignupSchema = joi.object({
     name: joi.string().min(2).required(),
     email: joi.string().email().required(),
     password: joi.string().required(),
-    specialty: joi.string().required,
-    location: joi.string().required
+    specialty: joi.string().required(),
+    location: joi.string().required(),
 });
 
 export const doctorLoginSchema = joi.object({
